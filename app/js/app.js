69a254f743e0e33445312a3dca91787b2035c0e8
Charlotka <c.roselnavarro@gmail.com>
2018-03-25 21:35:48 +0200

    #010 test to set controls app

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,4 @@
+var keyboard = {};
 
 // Scene
 var scene = new THREE.Scene();
@@ -20,7 +21,7 @@ window.addEventListener( 'resize', function( )
 
 } );
 
-controls = new THREE.OrbitControls( camera, renderer.domElement )
+// controls = new THREE.OrbitControls( camera, renderer.domElement )
 
 // Sun
 var sunGeo = new THREE.SphereGeometry( 20, 32, 32 );
@@ -31,9 +32,6 @@ sun.position.set(0,-15,0);
 sun.rotation.set(0.5,0.5,0);
 scene.add( sun );
 
-// var axis = new THREE.AxisHelper(100);
-// sun.add( axis );
-
 // Earth
 var earthGeo = new THREE.SphereGeometry( 8, 32, 32 );;
 var earthMat = new THREE.MeshPhongMaterial( { color: 0xaaaaaa, ambient: 0xaaaaaa, specular: 0x333333, shininess: 15, map: new THREE.TextureLoader( ).load( 'img/earth-texture.jpg' ), specularMap: new THREE.TextureLoader( ).load( 'img/earth-specular.png' ), normalMap: new THREE.TextureLoader( ).load( 'img/earth-normal.jpg' ) } );
@@ -42,21 +40,21 @@ earth.position.set( 75, 0, 0 );
 earth.rotation.set( 0, -0.25, 0.3 );
 sun.add( earth );
 
-
-// var axis2 = new THREE.AxisHelper(100);
-// earth.add( axis2 );
-
-
 // Moon
 var moonGeo = new THREE.SphereGeometry( 2, 32, 32 );
-var moonMat = new THREE.MeshPhongMaterial( { color: 0xaaaaaa, ambient: 0xffffff, specular: 0x333333, shininess: 0, map: new THREE.TextureLoader( ).load( 'img/moon-texture.jpg' ) } );
+var moonMat = new THREE.MeshPhongMaterial( { 
+	color: 0xaaaaaa, 
+	ambient: 0xffffff, 
+	specular: 0x333333, 
+	shininess: 0, 
+	map: new THREE.TextureLoader( ).load( 'img/moon-texture.jpg' )
+ } );
 var moon = new THREE.Mesh( moonGeo, moonMat );
 moon.position.x = 25;
 
 earth.add( moon );
 
-// Sunlights
-// sunlight.target = earth;
+// Lights
 var sunlight = new THREE.SpotLight( 0xffffff );
 sunlight.position.set( 100, 1000, 100 );
 sun.add(sunlight);
@@ -70,12 +68,34 @@ camera.position.set( 0, 0, 80 );
 
 // App Logic
 var update = function ( )
-{
-	
+{	
 	sun.rotation.y += 0.005;
 	earth.rotation.y += 0.015;
 	moon.rotation.y += 0.020;
+
+	if(keyboard[39]) {
+		camera.position.x -= 0.1;
+	}
+	if(keyboard[37]) {
+		camera.position.x += 0.1;
+	}
+	if(keyboard[38]) {
+		camera.position.z -= 0.1;
+	}
+	if(keyboard[40]) {
+		camera.position.z += 0.1;
+	}
 };
+
+
+function keyDown( event ){
+	keyboard[event.keyCode] = true;	
+}
+function keyUp( event ){
+	keyboard[event.keyCode] = false;
+}
+window.addEventListener( 'keydown', keyDown );
+window.addEventListener( 'keyup', keyUp );
 
 // draw Scene
 var render = function ( )
